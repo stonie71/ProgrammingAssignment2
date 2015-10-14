0d8e1215060e2b4a4fe46b1f484af1b907942307
Michael Stone <mjstone01@yahoo.com>
2015-10-14 22:01:40 +1030

    Changed vector mean to inverse matrix calculation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a matrix-like object that can cache its own inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	setmatrix <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+	getmatrix <- function() x
+	setinverse <- function(inverse) i <<- inverse
+	getinverse <- function() i
+	list(setmatrix = setmatrix, getmatrix = getmatrix, setinverse = 
+		setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the matrix-like object. Uses cache where possible. 
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+        	message("getting cached data")
+        	return(i)
+        }
+        data <- x$get()
+        i <- solve(x)
+        x$setinverse(i)
+        i
 }
+
